394000519ea1596357f23cd0531c39f57c4d68cf
Robin Bareham <Robin2.Bareham@live.uwe.ac.uk>
2026-09-19 13:09:34 +0100

    Added assets for rubbish and goals

--- Scripts/minigame.gd
+++ Scripts/minigame.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 
 @onready var v_goal = $Goal
+@onready var v_goal_CS2D = $Goal/Area2D/CollisionShape2D
 
 @onready var v_rubbish_list = []
 
 var v_goal_positions = [[401,361],[982,528],[523,792],[1611,354],[1400,811]]
-var v_rubbish_icons = ["temp_leaf1","temp_leaf2","temp_leaf3"]
+var v_rubbish_icons = ["Rubbish01","Rubbish02","Rubbish03","Rubbish04"]
 
 
 func _ready():
@@ -14,8 +15,11 @@ func _ready():
 
 func resetMinigame(goalTexture: String):
 	#Set goal's position 
-	var goalTxt_location = "res://Assets/TempAssets/" + goalTexture + ".png"
+	var goalTxt_location = "res://Assets/Minigame/" + goalTexture + ".png"
 	v_goal.texture = load(goalTxt_location)
+	#Change goal's Collision shape2D
+	var goalCS2D_loc = "res://Assets/Styles/" + goalTexture + "CS2D.tres"
+	v_goal_CS2D.shape = load(goalCS2D_loc)
 	v_goal.position.x = v_goal_positions[randi() % v_goal_positions.size()][0]
 	v_goal.position.y = v_goal_positions[randi() % v_goal_positions.size()][1]
 	#Run through list, set their position based on goal's position
@@ -24,9 +28,11 @@ func resetMinigame(goalTexture: String):
 		v_rubbish_list[i].position.x = v_goal.position.x + randi_range(-250,250)
 		v_rubbish_list[i].position.y = v_goal.position.y + randi_range(-250,250)
 		#Change sprite picture to random rubish
-		var texture_location = "res://Assets/TempAssets/" + v_rubbish_icons[randi() % v_rubbish_list.size()] + ".png"
+		var rand_index = randi() % v_rubbish_list.size()
+		var texture_location = "res://Assets/Minigame/" + v_rubbish_icons[rand_index] + ".png"
 		v_rubbish_list[i].texture = load(texture_location)
-		
+		var cs2d_loc = "res://Assets/Styles/" + v_rubbish_icons[rand_index] + "CS2D.tres"
+		v_rubbish_list[i].getCS2D().shape = load(cs2d_loc)
 		# MIGHT HAVE TO FIGURE OUT HOW TO CHANGE THE HITBOX BASED ON THE SPRITE CHOSEN
 		
 		pass
--- Scripts/minigame_rubbish.gd
+++ Scripts/minigame_rubbish.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 #From tutorial: https://youtu.be/3ThOxFZcie0?si=0gFSxajLTfjF_iE2
+@onready var v_cs2d = $Area2D/CollisionShape2D
 
 var v_dragging = false
 var v_offset = Vector2(0,0)
@@ -8,6 +9,9 @@ var v_offset = Vector2(0,0)
 func _process(delta):
 	if(v_dragging):
 		position = get_global_mouse_position() - v_offset
+
+func getCS2D():
+	return v_cs2d
 
 func _on_button_button_down() -> void:
 	v_dragging = true
