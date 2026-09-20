070c735a14487e986ae3201f43a6ef1911c78f83
Robin Bareham <Robin2.Bareham@live.uwe.ac.uk>
2026-09-20 12:56:01 +0100

    Added Bushes

--- Scripts/tree.gd
+++ Scripts/tree.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-@export var tree_type: String
-@onready var tree_sprite = $StaticBody2D/Sprite2D
+@export var type: String
+@onready var sprite = $StaticBody2D/Sprite2D
 
 func _ready():
-	var texture_location = "res://Assets/Sprites/" + tree_type + ".png"
-	tree_sprite.texture = load(texture_location)
+	var texture_location = "res://Assets/Sprites/" + type + ".png"
+	sprite.texture = load(texture_location)
